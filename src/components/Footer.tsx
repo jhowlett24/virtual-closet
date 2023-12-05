b576fda89cfe0574a5f84b5cabb5ec30a79daba7
jhowlett24 <jhowlett@syr.edu>
2023-12-04 19:13:34 -0500

    howie

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import "../CSS/Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-text"> hello</div>
+    </div>
+  );
+};
+export default Footer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,14 @@ import CarouselBottom2 from "../components/CarouselBottom2";
 import Carousel2 from "../components/Carousel2";
 import "../CSS/Home.css";
 import CarouselContainer from "../components/CarouselContainer";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div className="home">
       <NavBar />
       <CarouselContainer />
+      <Footer />
     </div>
   );
 };
